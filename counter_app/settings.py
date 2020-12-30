d716ec650ae09c41ffdbf23fd5af47dbdc6a91d7
Sanuja Methmal <methmal66@yahoo.com>
2020-12-30 17:20:19 +0530

    add icon and css to static folder

--- counter_app/settings.py
+++ counter_app/settings.py
@@ -130,6 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'client', 'static', 'client')
+STATIC_ROOT = os.path.join(BASE_DIR, 'client', 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
